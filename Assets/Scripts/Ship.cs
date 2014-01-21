1f4980d112828361ba03e3b60f88c46227fa1bd3
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-01-21 23:20:45 +0100

    ship finds its own bullet spawn point

--- Assets/Scripts/Ship.cs
+++ Assets/Scripts/Ship.cs
@@ -2,21 +2,25 @@
 using System.Collections;
 
 public class Ship : MonoBehaviour {
-	public bool btnLeftDown = false;
-	public bool btnRightDown = false;
+
 	public float tiltThreshold = .3f;
 	public float freezeTime = 0.2f;
 	public static float fireRate = .4f;//0.5217391f;
+
+	public GameObject explosion;
 	public GameObject bullet;
 	public int bulletSpeed = 200;
-	public Transform bulletSpawnTransform;
 	public GameObject[] spawns;
 
+	Transform bulletSpawnTransform;
+	GameController gameController;
 	SpawnController spawnController;
 	int currPos = 1;
 
 	void Start ()
 	{
+		bulletSpawnTransform = GameObject.Find ("BulletSpawnPoint").transform;
+		gameController = GameObject.Find ("GameController").GetComponent<GameController> ();
 		spawnController = GameObject.Find ("SpawnController").GetComponent<SpawnController> ();
 		spawns = spawnController.spawns;
 		StartCoroutine(Shoot());
@@ -72,4 +76,20 @@ public class Ship : MonoBehaviour {
 			"time", .4f
 			));
 	}
+
+	void OnCollisionEnter (Collision collision)
+	{
+		if (collision.gameObject.CompareTag("asteroid"))
+		{
+			gameController.points++;
+			// TODO Check for combo and multiply
+			gameController.TweenGameObject(gameController.pointsTxt);
+		}
+		
+		Destroy(collision.gameObject);
+		GameObject explosionInstance = (GameObject)Instantiate (explosion, collision.contacts[0].point, Quaternion.identity);
+		Destroy (explosionInstance, 3f);
+		Destroy (gameObject);
+	}
+
 }
