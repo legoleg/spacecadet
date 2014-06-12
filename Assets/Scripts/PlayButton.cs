adf95395b13c60933e1d55fbdef1705c0152dc5a
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-06-12 23:13:56 +0200

    in-editor debugging controls for menu scene

--- Assets/Scripts/PlayButton.cs
+++ Assets/Scripts/PlayButton.cs
@@ -6,11 +6,24 @@ public class PlayButton : TouchLogic {
 	void Update()
 	{
 		CheckTouches();
+
+#if UNITY_EDITOR
+		// get input from keyboard
+		if (Input.GetButton("Fire1"))
+		{
+			LoadGameScene (1);
+		}	
+#endif
 	}
 	
 	public override void OnTouchBegan()
 	{
 		Debug.Log("OnTouchBegan message received...");
-		Application.LoadLevel(1);
+		LoadGameScene (1);
+	}
+
+	static void LoadGameScene (int sceneID)
+	{
+		Application.LoadLevel (sceneID);
 	}
 }
