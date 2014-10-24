839af7e4712642cd2eadeee66986e82a036db145
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-10-24 17:17:02 +0200

    enable movement from swipeInput

--- Assets/Scripts/SwipeInput.cs
+++ Assets/Scripts/SwipeInput.cs
@@ -5,6 +5,12 @@ public class SwipeInput : MonoBehaviour {
 	public float minSwipeDistY;
 	public float minSwipeDistX;
 	private Vector2 startPos;
+
+	Ship ship;
+
+	void Start () {
+		ship = GameObject.FindGameObjectWithTag ("Player").GetComponent<Ship> ();
+	}
 	
 	void Update() {
 		if (Input.touchCount > 0) {
@@ -15,28 +21,16 @@ public class SwipeInput : MonoBehaviour {
 				startPos = touch.position;
 				break;
 			case TouchPhase.Ended:
-				float swipeDistVertical = (new Vector3(0, touch.position.y, 0) - new Vector3(0, startPos.y, 0)).magnitude;
-				if (swipeDistVertical > minSwipeDistY) {
-					float swipeValue = Mathf.Sign(touch.position.y - startPos.y);
-					//up swipe
-					if (swipeValue > 0) {
-						//Jump ();
-					} 
-					//down swipe
-					else if (swipeValue < 0) {
-						//Shrink ();
-					}
-				}
 				float swipeDistHorizontal = (new Vector3(touch.position.x,0, 0) - new Vector3(startPos.x, 0, 0)).magnitude;
 				if (swipeDistHorizontal > minSwipeDistX) {
 					float swipeValue = Mathf.Sign(touch.position.x - startPos.x);
 					//right swipe
 					if (swipeValue > 0) {
-						//MoveRight ();
+						ship.MoveRight();
 					}
 					//left swipe
 					else if (swipeValue < 0) {
-						//MoveLeft ();
+						ship.MoveLeft();
 					}
 				}
 				break;
