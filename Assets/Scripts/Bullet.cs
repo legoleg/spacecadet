fd156646508f8475c482cfd05bd501598ff7c9b5
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-10-26 23:29:06 +0100

    added variation to the bullet sounds

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,22 +4,32 @@ using System.Collections;
 public class Bullet : MonoBehaviour
 {
 	public GameObject explosion;
+	public AudioClip[] bulletSounds;
+	public AudioClip[] rewardSounds;
 
-	GameController gameController;
+	private GameController gameController;
 
 	void Start ()
 	{
 		gameController = GameObject.FindWithTag ("GameController").GetComponent<GameController>();
+
+		audio.pitch = Random.Range (0.9f, 1.1f);
+		if (bulletSounds.Length > 0) {
+			audio.PlayOneShot(bulletSounds [Random.Range(0, bulletSounds.Length)]);
+		}
 	}
 
 	void OnCollisionEnter2D (Collision2D collision)
 	{
 		if (collision.gameObject.tag == "asteroid") {
 			gameController.AddPoints(1);
+			if (rewardSounds.Length > 0) {
+				audio.PlayOneShot(rewardSounds[Random.Range(0, rewardSounds.Length)]);
+			}
 			Destroy(collision.gameObject);
 			GameObject explosionInstance = (GameObject)Instantiate (explosion, collision.contacts[0].point, Quaternion.identity);
 			Destroy (explosionInstance, 3f);
-			Destroy (gameObject);
+			Destroy (gameObject, audio.clip.length);
 		}
 	}
 }
