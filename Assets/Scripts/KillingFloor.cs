c174d13302346241bdb596b04894ccbab90492b7
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-10-26 22:50:25 +0100

    punish by loosing a point when a asteroid is missed

--- Assets/Scripts/KillingFloor.cs
+++ Assets/Scripts/KillingFloor.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class KillingFloor : MonoBehaviour {
 
+	GameController gameController;
+
+	void Start ()
+	{
+		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+
 	void OnCollisionEnter2D(Collision2D other)
 	{
+		if (other.gameObject.CompareTag("asteroid")) {
+			gameController.AddPoints(-1);
+		}
 		Destroy(other.gameObject);
 	}
 }
