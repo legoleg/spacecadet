975a1d657261c059ed4766b7b0c6b1b27134ed59
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-10-27 23:43:53 +0100

    follow finger script added

---
+++ Assets/Scripts/FollowFinger.cs
@@ -0,0 +1,33 @@
+﻿// Divides the Screen.width in 3 equal parts corresponding to the tracks.
+// When finger is moved, it checks which part the finger is in.
+// If the part is different than in the last frame, inscruct the ship of the new track
+
+
+
+using UnityEngine;
+using System.Collections;
+
+public class FollowFinger : MonoBehaviour {
+	
+	private Ship ship;
+	
+	void Start ()
+	{
+		ship = GameObject.FindGameObjectWithTag ("Player").GetComponent<Ship> ();
+
+	}
+	
+	void Update()
+	{
+		if (Input.touchCount > 0) {
+			Touch touch = Input.touches[0];
+			
+			switch (touch.phase) {
+			case TouchPhase.Began:
+				break;
+			case TouchPhase.Moved:
+				break;
+			}
+		}
+	}
+}
