ad3aaca501d0cf263c3f8d88b56d37427d95d066
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-11-03 20:24:31 +0100

    shooting with ship animation

---
+++ Assets/Scripts/ShipAnimationEvents.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipAnimationEvents : MonoBehaviour {
+	private Ship ship;
+
+	void Start () {
+		ship = GetComponentsInParent<Ship> ()[0];
+	}
+
+	public void Shoot () {
+		if (ship.canShoot) {
+			ship.Shoot();
+		}
+	}
+}
