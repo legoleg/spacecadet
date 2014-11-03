e0825e1bd74b8ecce13944c7b8ca46eb6f52cdc5
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-11-03 16:37:26 +0100

    ship follow finger script

--- Assets/Scripts/FollowFinger.cs
+++ Assets/Scripts/FollowFinger.cs
@@ -10,11 +10,22 @@ using System.Collections;
 public class FollowFinger : MonoBehaviour {
 	
 	private Ship ship;
+	private int lastSection;
+	private int currentSection = 1;
+	private int x1,x2;
+
 	
 	void Start ()
 	{
 		ship = GameObject.FindGameObjectWithTag ("Player").GetComponent<Ship> ();
 
+		// Divide screen in 3 sections
+		// If Screen.width is 480
+		// 0 = -inf,160 or <= Screen.width/3
+		// 1 = 161,320 or >Screen.width/3 && <(2*(Screen.width/3))
+		// 2 = 321,inf or >=(2*(Screen.width/3))
+		x1 = Screen.width / 3;
+		x2 = x1 * 2;
 	}
 	
 	void Update()
@@ -24,10 +35,29 @@ public class FollowFinger : MonoBehaviour {
 			
 			switch (touch.phase) {
 			case TouchPhase.Began:
+				MoveIfNewPosition(touch);
 				break;
 			case TouchPhase.Moved:
+				MoveIfNewPosition(touch);
 				break;
 			}
+		}
+	}
+
+	void MoveIfNewPosition (Touch touch)
+	{
+		lastSection = currentSection;
+		
+		if (touch.position.x < x1) {
+			currentSection = 0;
+		} else if (touch.position.x > x2) {
+			currentSection = 2;
+		} else {
+			currentSection = 1;
+		}
+		
+		if (lastSection != currentSection) {
+			ship.ApplyMove (currentSection);
 		}
 	}
 }
