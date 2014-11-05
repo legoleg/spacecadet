a165a92f04a3cfd88cbde0aeda96eb511a5478c0
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-11-05 21:21:55 +0100

    google plus icon will sign you in

--- Assets/Scripts/GooglePlayGamesController.cs
+++ Assets/Scripts/GooglePlayGamesController.cs
@@ -5,41 +5,28 @@ using UnityEngine.SocialPlatforms;
 public class GooglePlayGamesController : MonoBehaviour {
 	private const float FontSizeMult = 0.05f;
 	private bool mWaitingForAuth = false;
-	private string mStatusText = "Ready.";
-	
+
 	void Start () {
 		// Select the Google Play Games platform as our social platform implementation
 		GooglePlayGames.PlayGamesPlatform.Activate();
 	}
 	
-	void OnGUI() {
-		GUI.skin.button.fontSize = (int)(FontSizeMult * Screen.height);
-		GUI.skin.label.fontSize = (int)(FontSizeMult * Screen.height);
-		
-		GUI.Label(new Rect(20, 20, Screen.width, Screen.height * 0.25f), mStatusText);
-		
+	public void SignIn() {
 		if (mWaitingForAuth) {
 			return;
 		}
-		
-		string buttonLabel = Social.localUser.authenticated ? "Sign Out" : "Authenticate";
-		Rect buttonRect = new Rect(0.25f * Screen.width, 0.25f * Screen.height,
-		                           0.5f * Screen.width, 0.5f * Screen.height);
-		
-		if (GUI.Button(buttonRect, buttonLabel)) {
-			if (!Social.localUser.authenticated) {
-				// Authenticate
-				mWaitingForAuth = true;
-				mStatusText = "Authenticating...";
-				Social.localUser.Authenticate((bool success) => {
-					mWaitingForAuth = false;
-					mStatusText = success ? "Successfully authenticated" : "Authentication failed.";
-				});
-			} else {
-				// Sign out!
-				mStatusText = "Signing out.";
-				((GooglePlayGames.PlayGamesPlatform) Social.Active).SignOut();
-			}
+
+		if (!Social.localUser.authenticated) {
+			// Authenticate
+			mWaitingForAuth = true;
+			Social.localUser.Authenticate((bool success) => {
+				mWaitingForAuth = false;
+			});
+			// TODO add exiting animation that shows sign in process
+
+		} else {
+			// Sign out!
+			((GooglePlayGames.PlayGamesPlatform) Social.Active).SignOut();
 		}
 	}
 }
