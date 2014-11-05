64b0b3714f2584d60330b61267c98d4a3ef9080b
Nicolaj Schweitz <nicolaj.schweitz@gmail.com>
2014-11-06 00:51:30 +0100

    g+ signin

--- Assets/Scripts/GooglePlayGamesController.cs
+++ Assets/Scripts/GooglePlayGamesController.cs
@@ -1,14 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityEngine.SocialPlatforms;
 
 public class GooglePlayGamesController : MonoBehaviour {
 	private const float FontSizeMult = 0.05f;
 	private bool mWaitingForAuth = false;
+	public Sprite signedIn, waiting, signedOut;
 
 	void Start () {
 		// Select the Google Play Games platform as our social platform implementation
 		GooglePlayGames.PlayGamesPlatform.Activate();
+		GetComponent<Button>().image.sprite = signedOut;
+	}
+
+	void Update () {
+		SignIn();
 	}
 	
 	public void SignIn() {
@@ -19,14 +26,17 @@ public class GooglePlayGamesController : MonoBehaviour {
 		if (!Social.localUser.authenticated) {
 			// Authenticate
 			mWaitingForAuth = true;
+			// TODO add exiting animation that shows sign in process
+			GetComponent<Button>().image.sprite = waiting;
 			Social.localUser.Authenticate((bool success) => {
 				mWaitingForAuth = false;
+				GetComponent<Button>().image.sprite = signedIn;
 			});
-			// TODO add exiting animation that shows sign in process
-
-		} else {
-			// Sign out!
-			((GooglePlayGames.PlayGamesPlatform) Social.Active).SignOut();
-		}
+		} 
+//		else {
+//			// Sign out!
+//			((GooglePlayGames.PlayGamesPlatform) Social.Active).SignOut();
+//			GetComponent<Button>().image.sprite = signedOut;
+//		}
 	}
 }
